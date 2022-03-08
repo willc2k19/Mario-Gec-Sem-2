08e184aee93b2cdb1b93726dfe02044c098b293e
Will <53609817+willc2k19@users.noreply.github.com>
2022-03-08 15:04:47 +0000

    further SDL setup

--- MarioGEC/MarioGEC/MarioGEC.cpp
+++ MarioGEC/MarioGEC/MarioGEC.cpp
@@ -1,9 +1,65 @@
-
 #include <SDL.h>
 #include <SDL_image.h>
 #include <SDL_mixer.h>
+#include <iostream>
+#include "constants.h"
+using namespace std;
+
+//Globals
+SDL_Window* g_window = nullptr;
+
+//Function prototypes
+bool InitSDL();
+void CloseSDL();
 
 int main(int argc, char* args[])
 {
+	//check if sdl was setup correctly
+	if (InitSDL())
+	{
+		SDL_Delay(5000);
+	}
+
+	CloseSDL();
+
 	return 0;
+}
+
+bool InitSDL()
+{
+	//Setup SDL
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		cout << "SDL did not initialise. Error: " << SDL_GetError();
+		return false;
+	}
+	else
+	{
+		//setup passed so create window
+		g_window = SDL_CreateWindow("Games Engine Creation",
+			SDL_WINDOWPOS_UNDEFINED,
+			SDL_WINDOWPOS_UNDEFINED,
+			SCREEN_WIDTH,
+			SCREEN_HEIGHT,
+			SDL_WINDOW_SHOWN);
+		//did the window get created?
+		if (g_window == nullptr)
+		{
+			//window failed
+			cout << "Window was not created. Error: " << SDL_GetError();
+			return false;
+		}
+		return true;
+	}
+}
+
+void CloseSDL()
+{
+	//release the window
+	SDL_DestroyWindow(g_window);
+	g_window = nullptr;
+
+	//quit SDL subsystems
+	IMG_Quit();
+	SDL_Quit();
 }
